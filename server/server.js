bb6eb351f27efc2292c6606c7d2a6a86fecbb6ce
Helen Lu <helenlu1997@gmail.com>
2018-10-31 14:34:53 -0400

    server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const app = express()
 const port = 7000
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join('../index.html'));
+    res.sendFile('../index.html');
 });
 app.listen(port, () => console.log(`App listening on port ${port}!`))
