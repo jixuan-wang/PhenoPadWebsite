0b877727b36df8dce0e54cf4c5d063617ba8bb3c
Helen Lu <helenlu1997@gmail.com>
2018-10-31 15:21:06 -0400

    server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 7000
 const path = '/home/hnakxr/PhenoPadWebsite/'
+
+
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.get('/', function(req, res) {
-    res.send('listening');
+    res.send(__dirname+'index.html');
 });
 app.listen(port, () => console.log(`App listening on port ${port}!`))
