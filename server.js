affedc5efacaa0675ebc1777e826853244331919
Helen Lu <helenlu1997@gmail.com>
2018-11-01 17:28:40 -0400

    added more contents

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 7000
 const path = '/home/hnakxr/PhenoPadWebsite/'
 
 app.use(express.static(__dirname + '/vendor'))
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/device-mockups'))
 app.get('/', function(req, res) {
     res.send(__dirname+'/index.html')
 });
