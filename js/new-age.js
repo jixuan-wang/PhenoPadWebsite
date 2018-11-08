17d80e73ed62b716d345bc4415fe1cb3a08cf427
Helen <helenlu1997@gmail.com>
2018-11-08 16:05:22 -0500

    Layout updates

--- js/new-age.js
+++ js/new-age.js
@@ -25,10 +25,10 @@
     target: '#mainNav',
     offset: 54
   });
-
+  var window_height = $(window).height;
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
+    if ($("#mainNav").offset().top > window_height) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
