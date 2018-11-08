c8616d7dab2e4c16a333382a6f53b237661d09ad
Helen <helenlu1997@gmail.com>
2018-11-08 16:36:06 -0500

    server updates

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express()
 const port = 7000
 const path = '/home/hnakxr/PhenoPadWebsite/'
 
-app.use(express.static(__dirname + '/vendor'))
-app.use(express.static(__dirname + '/device-mockups'))
+app.use(express.static(__dirname ))
+// app.use(express.static(__dirname + '/css'))
+// app.use(express.static(__dirname + '/device-mockups'))
+// app.use(express.static(__dirname + '/vendor'))
+
 app.get('/', function(req, res) {
     res.send(__dirname+'/index.html')
 });
