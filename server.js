7278aadfffce48e6853d8c91c153568b54d98df2
Helen Lu <helenlu1997@gmail.com>
2019-01-28 15:55:55 -0500

    1

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const port = 80
 const path = '/home/hnakxr/PhenoPadWebsite/'
 
 app.use(express.static(__dirname ))
-// app.use(express.static(__dirname + '/css'))
-// app.use(express.static(__dirname + '/device-mockups'))
-// app.use(express.static(__dirname + '/vendor'))
+app.use(express.static(__dirname + '/css'))
+app.use(express.static(__dirname + '/device-mockups'))
+app.use(express.static(__dirname + '/vendor'))
 
 app.get('/', function(req, res) {
     res.send(__dirname+'/index.html')
