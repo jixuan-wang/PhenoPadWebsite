7f19fbe2bff83f54f482886de81b4e3869f2d889
Helen Lu <helenlu1997@gmail.com>
2019-01-28 15:49:02 -0500

    1

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/', function(req, res) {
     res.send(__dirname+'/index.html')
 });
 app.get("/m", function(req, res) {
-    res.send(__dirname+'/manual.html')
+    res.send(__dirname+'/index.html')
 });
 app.listen(port, () => console.log(`App listening on port ${port}!`))
